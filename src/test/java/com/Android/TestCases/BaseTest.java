304e65e2a085aaeb22a21bf9cc9fea97030c135b
varun-mr <68236252+varun-mr@users.noreply.github.com>
2021-09-15 13:21:08 +0530

    Added BeforeTest method

--- src/test/java/com/Android/TestCases/BaseTest.java
+++ src/test/java/com/Android/TestCases/BaseTest.java
@@ -1,10 +1,17 @@
 package com.Android.TestCases;
 
 import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeTest;
 
 import com.Android.Base.BaseClass;
 
 public class BaseTest {
+
+	@BeforeTest
+	public void KillNodeProcesses() throws IOException, InterruptedException {
+		Runtime.getRuntime().exec("taskkill /F /IM node.exe");
+		Thread.sleep(3000);
+	}
 	
 	@AfterSuite
 	public void end() {
